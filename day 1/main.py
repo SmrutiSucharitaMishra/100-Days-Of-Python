15bcb26871abe559c19abc0fcf884851b1f97fec
SmrutiSucharitaMishra <97398987+SmrutiSucharitaMishra@users.noreply.github.com>
2022-01-09 21:45:22 +0530

    Day 1 Project
    Predicting Band name Project added. Day 1 of 100 days of python

---
+++ day 1/main.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for your program.
+print("welcome to the bandname generator")
+#2. Ask the user for the city that they grew up in.
+city=input("what is the name of your city you grown up\n")
+#3. Ask the user for the name of a pet.
+pet=input("what is the name of your pet\n ")
+#4. Combine the name of their city and pet and show them their band name.
+print("the name of the band is   "+city+"  "+pet)
